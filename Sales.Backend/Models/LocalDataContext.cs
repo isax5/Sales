d52fa1e2fbf75b7fb9c1303763a2a5029a7e1183
isax5 <isaac.rebolledoleal@gmail.com>
2019-03-27 16:32:56 -0300

    Servicios publicados y controladores creados

---
+++ Sales.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Sales.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sales.Backend.Models
+{
+    /// <summary>
+    /// Herencia de <see cref="DataContext"/> para facilitar las migraciones
+    /// </summary>
+    public class LocalDataContext : DataContext
+    {
+        // TODO: Para API usamos el DataContext de Domain, entoces quiero ver que pasa con este que sobre
+        // escribe algo que ya tienen pero se usaran migraciones
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+    }
+}
